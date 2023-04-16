81d6645df5c1b205e20964a8d431df3e6ca68f41
agustinmfran <agustinmfran@gmail.com>
2023-04-16 11:04:25 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Main from "./components/Main";
-import "./index.css";
+import React from "react";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
